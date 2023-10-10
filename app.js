a2399a1ce321725beb9361aec6a220abeeabeb2a
Ayush <ayushguptass14@gmail.com>
2023-10-10 14:59:20 +0530

    done

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
+const sql = require("mssql");
 require('dotenv').config();
 
-const port = process.env.PORT || 3000; // Use 3000 as the default if PORT is not defined
+const port = process.env.PORT || 3400; // Use 3000 as the default if PORT is not defined
 
 // Define a route
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://musical-snickerdoodle-08c739.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://musical-snickerdoodle-08c739.netlify.app' );
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
@@ -20,9 +21,58 @@ app.get('/', (req, res) => {
 app.get('/ayush', (req, res)=>{
     res.send("ayush")
 })
-app.get('/about', (req, res)=>{
-  res.send({"name":"ayush gupta"})
-})
+
+const config = {
+  user: "Ayush", // better stored in an app setting such as process.env.DB_USER
+  password: "Data@12345", // better stored in an app setting such as process.env.DB_PASSWORD
+  server: "render.database.windows.net", // better stored in an app setting such as process.env.DB_SERVER
+  port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
+  database: "Parking", // better stored in an app setting such as process.env.DB_NAME
+  authentication: {
+    type: "default",
+  },
+  options: {
+    encrypt: true,
+  },
+};
+
+async function connectAndQuery() {
+  try {
+    var poolConnection = await sql.connect(config);
+
+    // console.log("Reading rows from the Table...");
+    // var resultSet = await poolConnection.request().query(`SELECT * from test`);
+
+    // console.log(resultSet.recordset[0].id);
+
+    // const arg="select * from test";
+    // var ress=await poolConnection.request().query(arg);
+    // console.log(ress);
+
+    // // output column headers
+    console.log("connected....");
+    // // close connection only when we're certain application is finished
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+app.get("/about", async (req, res) => {
+  await connectAndQuery();
+  sql.query("select * from test", (error, data) => {
+    if (error) {
+      console.log("error");
+      console.log(error);
+      res.send({ error: "error" });
+      return;
+    } else {
+      res.send({ name: `${data.recordset[1].names}` });
+    }
+  });
+});
+
+
+
+
 
 // Start the server
 app.listen(port, () => {
